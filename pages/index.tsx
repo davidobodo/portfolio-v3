52fc9ab169f5a2a93f5f67ae9a318ad6e30afc75
David Obodo <obododavid5@gmail.com>
2022-06-05 15:12:53 +0100

    🎨STYLE: Preloader layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.scss";
+import styles from "../styles/globals.module.scss";
 import { Preloader } from "#/components";
 
 const Home: NextPage = () => {
