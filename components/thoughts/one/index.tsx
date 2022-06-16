625371d5880ab024c50ca1767b921ac5779df968
David Obodo <obododavid5@gmail.com>
2022-06-16 16:01:33 +0100

    🛠REFACTOR: Thoughts one layout to suite mobile design

--- components/thoughts/one/index.tsx
+++ components/thoughts/one/index.tsx
@@ -9,20 +9,8 @@ export default function FirstThought({ textWrapperRef }: { textWrapperRef: Ref<H
                 <div className={styles.paragraph}>
                     <div className={styles.textLine}>
                         <span>
-                            The things we make and the <strong> quality of the work </strong> we do{" "}
-                        </span>
-                    </div>
-
-                    <div className={styles.textLine}>
-                        <span>
-                            reveal someting
-                            <strong> about who we are </strong>, so I always strive to
-                        </span>
-                    </div>
-
-                    <div className={styles.textLine}>
-                        <span>
-                            put
+                            The things we make and the <strong> quality of the work </strong> we do reveal someting
+                            <strong> about who we are </strong>, so I always strive to put
                             <strong> excellence into it</strong> .
                         </span>
                     </div>
@@ -32,19 +20,9 @@ export default function FirstThought({ textWrapperRef }: { textWrapperRef: Ref<H
                     <div className={styles.textLine}>
                         <span>
                             {" "}
-                            From writing <strong> clean and scalable code </strong> for posterity sake, to
-                        </span>
-                    </div>
-
-                    <div className={styles.textLine}>
-                        <span>
-                            using the most <strong>efficient algorithms </strong> to ensure
-                        </span>
-                    </div>
-
-                    <div className={styles.textLine}>
-                        <span>
-                            <strong>optimal performance because &quot;Speed is king&quot;</strong>
+                            From writing <strong> clean and scalable code </strong> for posterity sake, to using the
+                            most <strong>efficient algorithms </strong> to ensure
+                            <strong> optimal performance because &quot;Speed is king&quot;</strong>
                         </span>
                     </div>
                 </div>
