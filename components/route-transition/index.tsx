48e84dc807072349ed8bc6b106fed30ba9e49ac0
David Obodo <obododavid5@gmail.com>
2022-07-09 16:18:00 +0100

    ✨FEATURE: Create router transition

--- components/route-transition/index.tsx
+++ components/route-transition/index.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
 import styles from "./styles.module.scss";
 import gsap from "gsap";
-import { Logo, Noise } from "#/components";
+import { Noise } from "#/components";
 
 export default function Routetransition() {
     const router = useRouter();
@@ -11,53 +11,35 @@ export default function Routetransition() {
     const layersWrapperRef = useRef<HTMLDivElement>(null);
     const noiseRef = useRef<HTMLDivElement>(null);
 
-    function aniStart() {
-        // const tl = gsap.timeline();
-
-        // tl.to(containerRef.current, {
-        //     scaleY: 1
-        // });
-
-        // tl.then(() => {
-        //     tl.reverse();
-        // });
-
-        displayLoader();
-    }
-
-    function aniEnd() {}
+    const [tl, setTl] = useState<gsap.core.Timeline>();
 
     function displayLoader() {
         const tl = gsap.timeline();
 
-        tl.to(layersWrapperRef.current?.children[0], { scaleY: 1 })
-            .to(layersWrapperRef.current?.children[1], { scaleY: 1 }, "<")
-            .to(noiseRef.current, {
-                opacity: 1,
-                visibility: "visible"
-            });
+        if (layersWrapperRef.current) {
+            tl.to(layersWrapperRef.current?.children[0], { scaleY: 1 })
+                .to(layersWrapperRef.current?.children[1], { scaleY: 1 }, "<")
+                .to(noiseRef.current, {
+                    opacity: 1,
+                    visibility: "visible"
+                });
+            setTl(tl);
 
-        tl.then(() => {
-            tl.reverse();
-        });
+            tl.then(() => {
+                tl.reverse();
+            });
+        }
     }
 
-    function removeLoader() {}
-
     useEffect(() => {
-        router.events.on("routeChangeStart", aniStart);
-        router.events.on("routeChangeComplete", aniEnd);
-        router.events.on("routeChangeError", aniEnd);
+        router.events.on("routeChangeStart", displayLoader);
         return () => {
-            router.events.off("routeChangeStart", aniStart);
-            router.events.off("routeChangeComplete", aniEnd);
-            router.events.off("routeChangeError", aniEnd);
+            router.events.off("routeChangeStart", displayLoader);
         };
     }, [router]);
 
     return (
-        <div className={styles.container} ref={containerRef} djak="route-wrapper">
-            {/* <button onClick={displayLoader}>PLAY WHAT I WANT</button> */}
+        <div className={styles.container} ref={containerRef}>
             <Noise noiseRef={noiseRef} containerStyles={{ zIndex: 1001, opacity: 0, visibility: "hidden" }} />
             <div className={styles.layers} ref={layersWrapperRef}>
                 <div className={styles.layer}></div>
