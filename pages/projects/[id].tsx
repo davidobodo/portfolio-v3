8370d62fd6f8a3c14ab3807761370bc67e57dcc5
Obodo David <obododavid5@gmail.com>
2022-08-02 16:43:28 +0100

    ✨FEATURE: Redirecting back to projects page from single project page

--- pages/projects/[id].tsx
+++ pages/projects/[id].tsx
@@ -5,64 +5,64 @@ import { useEffect } from "react";
 import styles from "#/styles/_pages/single-project.module.scss";
 import { useSelectProjectAnimation, removeCurrentProject, usePageTransition, useSingleProjectInit } from "#/hooks";
 export default function Project() {
-    useSingleProjectInit();
-    const router = useRouter();
-    const { id } = router.query;
-    const { onRouteChange } = usePageTransition();
-    const {
-        selectedProjectId,
-        // onDeselectProject,
-        modalImgRef,
-        modalRef,
-        onGoToProject,
-        setSelectedProjectId
-    } = useSelectProjectAnimation();
+	useSingleProjectInit();
+	const router = useRouter();
+	const { id } = router.query;
+	const { onRouteChange } = usePageTransition();
+	const {
+		selectedProjectId,
+		// onDeselectProject,
+		modalImgRef,
+		modalRef,
+		onGoToProject,
+		setSelectedProjectId,
+	} = useSelectProjectAnimation();
 
-    const onDeselectProject = () => {
-        if (modalRef.current && modalImgRef.current) {
-            const tl = removeCurrentProject({
-                modalContainer: modalRef.current,
-                modalImage: modalImgRef.current
-            });
+	const onDeselectProject = () => {
+		if (modalRef.current && modalImgRef.current) {
+			const tl = removeCurrentProject({
+				modalContainer: modalRef.current,
+				modalImage: modalImgRef.current,
+			});
 
-            tl.to(modalRef.current, { opacity: 0 });
-            tl.then(() => {
-                setSelectedProjectId("");
-                onRouteChange("/projects");
-            });
-        }
-    };
+			tl.to(modalRef.current, { opacity: 0 });
 
-    useEffect(() => {
-        if (id) {
-            setSelectedProjectId(id as string);
-        }
-    }, [id]);
+			tl.then(() => {
+				onRouteChange("/projects");
+			});
+		}
+	};
 
-    if (!id) {
-        return null;
-    }
+	useEffect(() => {
+		if (id) {
+			setSelectedProjectId(id as string);
+		}
+	}, [id]);
 
-    return (
-        <div className={styles.main}>
-            <Head>
-                <title>David Obodo</title>
-                <meta name="description" content="David Obodo's portfolio website" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <Nav alwaysVisible={true} />
-            <Layout.DarkSection>
-                <div ref={modalRef} className={styles.container}>
-                    <SingleProject
-                        currProjectId={selectedProjectId as string}
-                        // onClose={onClose}
-                        onClose={onDeselectProject}
-                        modalImgRef={modalImgRef}
-                        onGoToProject={onGoToProject}
-                    />
-                </div>
-            </Layout.DarkSection>
-            <Noise />
-        </div>
-    );
+	if (!id) {
+		return null;
+	}
+
+	return (
+		<div className={styles.main}>
+			<Head>
+				<title>David Obodo</title>
+				<meta name="description" content="David Obodo's portfolio website" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Nav alwaysVisible={true} />
+			<Layout.DarkSection>
+				<div ref={modalRef} className={styles.container}>
+					<SingleProject
+						currProjectId={selectedProjectId as string}
+						// onClose={onClose}
+						onClose={onDeselectProject}
+						modalImgRef={modalImgRef}
+						onGoToProject={onGoToProject}
+					/>
+				</div>
+			</Layout.DarkSection>
+			<Noise />
+		</div>
+	);
 }
