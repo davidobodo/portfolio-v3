4194e5d2c001aa3ea0889abef2a8badabd44e77a
Obodo David <obododavid5@gmail.com>
2022-09-11 20:30:38 +0100

    🛠FIX: Meta tags

--- pages/credits.tsx
+++ pages/credits.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import styles from "#/styles/_pages/credits.module.scss";
-import { Noise, Nav, Layout, Banners, BannerCurtain, AlternatingOpacity, Contact, HeadChildren } from "#/components";
+import { Noise, Nav, Layout, Banners, BannerCurtain, AlternatingOpacity, Contact } from "#/components";
 import { useGenericPageInit, useWindowSize, useAlternateTextOpacity, useIsomorphicLayoutEffect } from "#/hooks";
 import { useRef } from "react";
 import { otherSharedAnimations } from "#/utils/animations";
@@ -88,10 +88,37 @@ export default function Credit() {
 	});
 	const { textsListRef } = useAlternateTextOpacity();
 
+	const { title, description, url, image } = METADATA["credits"];
+
 	return (
 		<>
 			<Head>
-				<HeadChildren {...METADATA["credits"]} />
+				<title>{title}</title>
+				<meta charSet="utf-8" />
+				<meta property="type" content="website" />
+				<meta property="url" content={url} />
+				<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+				<meta name="theme-color" content="#e1dfdd" />
+
+				<meta property="title" content={title} />
+				<meta name="description" content={description} />
+				<meta property="image" content={image} />
+				<meta content="image/*" property="og:image:type" />
+
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content={title} />
+				<meta property="og:description" content={description} />
+				<meta property="og:url" content={url} />
+				<meta property="og:image" content={image} />
+				<meta property="og:site_name" content={title} />
+
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@phitGeek" />
+				<meta name="twitter:title" content={title} />
+				<meta name="twitter:description" content={description} />
+				<meta name="twitter:image" content={image} />
+
+				<link rel="icon" href="/icon-192x192.png" />
 			</Head>
 			<Nav hasBackdropFilter={false} />
 
