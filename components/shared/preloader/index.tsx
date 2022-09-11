b57bc9ec3218a70dc2160215a414333a9c184f6c
Obodo David <obododavid5@gmail.com>
2022-09-11 20:20:04 +0100

    ✨FEATURE: Change page loader

--- components/shared/preloader/index.tsx
+++ components/shared/preloader/index.tsx
@@ -2,8 +2,11 @@ import styles from "./styles.module.scss";
 
 export default function PreLoader() {
 	return (
-		<div id="blocker" className={styles.percentLoader}>
-			<Ellipsis />
+		<div id="blocker" className={styles.wrapper}>
+			<div className={styles.container}>
+				<p>Loading Assets</p>
+				<Ellipsis />
+			</div>
 		</div>
 	);
 }
